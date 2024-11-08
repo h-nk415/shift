ed70dded04ddb8d1ec3b151a5ae4fabe48edf6d6
3031249 <3031249@30312-49>
2024-11-08 11:49:56 +0900

    カレンダー形式に変更

--- src/main/java/com/example/shift/controller/ShiftsController.java
+++ src/main/java/com/example/shift/controller/ShiftsController.java
@@ -1,5 +1,7 @@
 package com.example.shift.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -25,143 +27,97 @@ import lombok.RequiredArgsConstructor;
 @ControllerAdvice
 public class ShiftsController {
 
-	private final ShiftsService shiftsService;
+    private final ShiftsService shiftsService;
 
-	//ホーム画面
-	@GetMapping("/home")
-	public String home(Model model) {
-		model.addAttribute("home", shiftsService.getAllShifts());
-		return "shift/home";
-	}
-	
-	//IDの詳細表示
-	@GetMapping("/{id}")
-	public String detail(@PathVariable Integer id, Model model, RedirectAttributes attributes) {
-		Shifts shift = shiftsService.getShiftById(id);
-		if(shift != null) {
-			model.addAttribute("shift", shift);
-			return "shift/detail";
-		}else {
-			//データがない場合フラッシュメッセージを表示
-			attributes.addFlashAttribute("message", "対象データがありません");
+    //ホーム画面
+    @GetMapping("/home")
+    public String home(Model model) {
+        LocalDate currentDate = LocalDate.now();
+        model.addAttribute("currentDate", currentDate);
+        model.addAttribute("home", shiftsService.getAllShifts());
+        return "shift/home";
+    }
+    
+    //IDの詳細表示
+    @GetMapping("/{id}")
+    public String detail(@PathVariable Integer id, Model model, RedirectAttributes attributes) {
+        Shifts shift = shiftsService.getShiftById(id);
+        if(shift != null) {
+            model.addAttribute("shift", shift);
+            return "shift/detail";
+        } else {
+            attributes.addFlashAttribute("message", "対象データがありません");
+            return "redirect:/shift/home";
+        }
+    }
 
-			//PRGパターン
-			return "redirect:/shift/home";
-		}
-	}
+    //登録画面の表示
+    @GetMapping("/form")
+    public String form(@ModelAttribute ShiftForm form) {
+        form.setIsNew(true);
+        return "shift/form";
+    }   
 
-	//登録画面の表示
-	@GetMapping("/form")
-	public String form(@ModelAttribute ShiftForm form) {
-		form.setIsNew(true);
-		return "shift/form";
-	}	
+    //登録実行
+    @PostMapping("/save")
+    public String save(@Validated ShiftForm form, BindingResult bindingResult, RedirectAttributes attributes) {
+        if(bindingResult.hasErrors()) {
+            form.setIsNew(true);
+            return "shift/form";
+        }
 
+        if (!form.isStartTimeBeforeEndTime()) {
+            bindingResult.rejectValue("startHour", "error.startTime", "開始時間は終了時間よりも前でなければなりません");
+            form.setIsNew(true);
+            return "shift/form";
+        }
 
+        Shifts shift = ShiftHelper.convertShift(form);
+        shiftsService.saveShift(shift);
+        attributes.addFlashAttribute("message", "シフトが登録されました");
+        return "redirect:/shift/home";
+    }
 
-	//登録実行
-	@PostMapping("/save")
-	public String save(@Validated ShiftForm form, BindingResult bindingResult, RedirectAttributes attributes) {
+    //修正画面の表示
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model, RedirectAttributes attributes) {
+        Shifts target = shiftsService.getShiftById(id);
+        
+        if(target != null) {
+            ShiftForm form = ShiftHelper.convertShiftForm(target);
+            model.addAttribute("shiftForm", form);
+            return "shift/form";
+        } else {
+            attributes.addFlashAttribute("message", "対象データがありません");
+            return "redirect:/shift/home";
+        }
+    }
 
-		//入力チェック
-		if(bindingResult.hasErrors()) {
-			form.setIsNew(true);
+    //修正実行
+    @PostMapping("/update")
+    public String update(@Validated ShiftForm form, BindingResult bindingResult,  RedirectAttributes attributes) {
+        if(bindingResult.hasErrors()) {
+            form.setIsNew(false);
+            return "shift/form";
+        }
 
-			return "shift/form";
-		}
+        if (!form.isStartTimeBeforeEndTime()) {
+            bindingResult.rejectValue("startHour", "error.startTime", "開始時間は終了時間よりも前でなければなりません");
+            form.setIsNew(false);
+            return "shift/form";
+        }
 
-		// 開始時間と終了時間のチェック
-		if (!form.isStartTimeBeforeEndTime()) {
-			bindingResult.rejectValue("startHour", "error.startTime", "開始時間は終了時間よりも前でなければなりません");
-			form.setIsNew(true);
-			return "shift/form";
-		}
+        Shifts shift = ShiftHelper.convertShift(form);
+        shiftsService.updateShift(shift);
+        attributes.addFlashAttribute("message", "シフトが更新されました");
+        return "redirect:/shift/home";
+    }
 
-
-		//エンティティへ変換
-		Shifts shift = ShiftHelper.convertShift(form);
-
-		//登録実行
-		shiftsService.saveShift(shift);
-
-		//フラッシュメッセージ
-		attributes.addFlashAttribute("message", "シフトが登録されました");
-
-		//PRGパターン
-		return "redirect:/shift/home";
-	}
-
-
-	//修正画面の表示
-	@GetMapping("/edit/{id}")
-	public String edit(@PathVariable Integer id, Model model, RedirectAttributes attributes) {
-
-		//IDに対応するシフトを取得
-		Shifts target = shiftsService.getShiftById(id);
-		
-
-
-		if(target != null) {
-			//データがあればフォームに変換
-			ShiftForm form = ShiftHelper.convertShiftForm(target);
-
-			//モデルに格納
-			model.addAttribute("shiftForm", form);
-			return "shift/form";
-		} else {
-			//データがない場合フラッシュメッセージを表示
-			attributes.addFlashAttribute("message", "対象データがありません");
-
-			//PRGパターン
-			return "redirect:/shift/detail";
-		}
-
-	}
-
-	//修正実行
-	@PostMapping("/update")
-	public String update(@Validated ShiftForm form, BindingResult bindingResult,  RedirectAttributes attributes) {
-
-		//入力チェック
-		if(bindingResult.hasErrors()) {
-			form.setIsNew(false);
-			return "shift/form";
-		}
-
-		// 開始時間と終了時間のチェック
-		if (!form.isStartTimeBeforeEndTime()) {
-			bindingResult.rejectValue("startHour", "error.startTime", "開始時間は終了時間よりも前でなければなりません");
-			form.setIsNew(false);
-			return "shift/form";
-		}
-
-		//エンティティへ変換
-		Shifts shift = ShiftHelper.convertShift(form);
-
-		//更新実行
-		shiftsService.updateShift(shift);
-
-		//フラッシュメッセージ
-		attributes.addFlashAttribute("message", "シフトが更新されました");
-
-		//PRGパターン
-		return "redirect:/shift/home";
-	}
-
-	//削除
-	@PostMapping("/delete/{id}")
-	public String delete(@PathVariable Integer id, Model model, RedirectAttributes attributes) {
-
-		//IDに対応するシフトを削除
-		shiftsService.deleteShift(id);
-
-		//フラッシュメッセージ
-		attributes.addFlashAttribute("message", "シフトが削除されました");
-
-		//PRGパターン
-		return "redirect:/shift/home";
-
-
-	}
-
+    //削除
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id, RedirectAttributes attributes) {
+        shiftsService.deleteShift(id);
+        attributes.addFlashAttribute("message", "シフトが削除されました");
+        return "redirect:/shift/home";
+    }
 }
